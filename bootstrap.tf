4caaa8cbeffa17401cfc6d43e6b1f75469685fbc
bogdanj22 <bogdanbodom@hotmail.com>
2025-08-22 22:22:45 +0200

    lock fix

--- bootstrap.tf
+++ bootstrap.tf
@@ -2,14 +2,6 @@ resource "aws_s3_bucket" "tf_state" {
   bucket = "bogdan-terraform-state-bucket"
   acl    = "private"
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
   tags = {
     Name = "Terraform State Bucket"
   }
@@ -20,5 +12,15 @@ resource "aws_s3_bucket_versioning" "tf_state_versioning" {
 
   versioning_configuration {
     status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
