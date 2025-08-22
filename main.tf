693b98c06bf8d72d3c2c447091eb04858ee487b4
bogdanj22 <bogdanbodom@hotmail.com>
2025-08-22 21:39:54 +0200

    subnet id output

--- main.tf
+++ main.tf
@@ -100,3 +100,15 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+variable "vpc_id" {}
+
+data "aws_vpc" "custom_vpc" {
+  id = var.vpc_id
+}
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id            = data.aws_vpc.custom_vpc.id
+  availability_zone = "eu-central-1a"
+  cidr_block        = cidrsubnet(data.aws_vpc.custom_vpc.cidr_block, 4, 1)
+}
