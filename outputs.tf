b64342d1985cdccf7bc4fc0f027894351c94e5e4
bogdanj22 <bogdanbodom@hotmail.com>
2025-08-28 10:20:02 +0200

    try

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "rds_endpoint" {
   value = aws_db_instance.postgres_rds.endpoint
 }
 
-# output "public_subnet_a_id" {
-#   description = "ID of the public subnet A"
-#   value       = aws_subnet.public_subnet_a.id
-# }
+output "public_subnet_a_id" {
+  description = "ID of the public subnet A"
+  value       = aws_subnet.public_subnet_a.id
+}
